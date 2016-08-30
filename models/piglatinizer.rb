fd595fdf5549f846e532d13a1dce59ebaa874850
Bibi Khadiza <limakhadiza@yahoo.com>
2016-08-30 15:30:49 -0400

    well that was a tuff one

---
+++ models/piglatinizer.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+
+class PigLatinizer
+
+  attr_accessor :word
+
+  def to_pig_latin(text)
+    text.split(" ").collect do |word|
+      new_string = piglatinize(word)
+    end.join(" ")
+  end
+
+
+
+  def piglatinize(word)
+    if !(word.start_with?("a", "e", "i", "o", "u", "A", "E", "I", "O", "U"))
+      let = /^[^aeiou]+/.match(word).to_s
+      new_word = word.gsub(/^#{let}/, "") + let + "ay"
+    elsif(word.start_with?("a", "e", "i", "o", "u", "A", "E", "I", "O", "U"))
+      new_word = word + "way"
+    end
+    new_word
+  end
+
+
+end
