1512d9a155b1325ee8625459d8e91c0ff7f789ad
Ekaterina Petushova <epetushova@gmail.com>
2016-11-24 13:59:27 -0500

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,29 @@
+class PigLatinizer
+
+	def piglatinize(word)
+    	vowels = [ 'a', 'e', 'i', 'o', 'u']
+    
+    	if vowels.include?(word[0].downcase) 
+      		pig_latin = word + ("way")
+    	else
+      	until vowels.include?(word[0].downcase) do
+        	word = word + word[0]
+        	word.gsub!(/^[^aeiou]/, '')
+      	end
+     		 pig_latin = word + ("ay")
+    	end
+    	pig_latin
+  	end
+
+ 	def to_pig_latin(string)
+    	sentence = []
+    	words_array = string.split(" ")
+    	words_array.each do |w|
+      	sentence << piglatinize(w)
+    	end
+    	sentence.join(" ")
+  	end
+end
+
+w = PigLatinizer.new
+w.piglatinize("I")
